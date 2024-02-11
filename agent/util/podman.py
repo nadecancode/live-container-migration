51b03a1a926de7e65612302a16ae14de603b4f0d
nade <i@nade.me>
2024-02-11 12:32:10 -0800

    Progress

--- agent/util/podman.py
+++ agent/util/podman.py
@@ -4,7 +4,6 @@ from urllib.parse import quote
 podman_sock_uri = f"http+unix://{quote('/run/podman/podman.sock', safe='')}"
 
 def call_podman(path, **kwargs):
-    print(f"{podman_sock_uri}{path}")
     return requests_unixsocket.request(
         url=f"{podman_sock_uri}{path}",
         **kwargs
